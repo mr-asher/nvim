3eb78a25618ac8f3b5d524d0051bba16364622ee
Asher <jasherleslie@gmail.com>
2024-07-01 12:31:24 +1000

    Better conform.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -32,6 +32,7 @@ return {
 				-- You can use a sub-list to tell conform to run *until* a formatter
 				-- is found.
 				javascript = { { "prettierd", "prettier" } },
+				htmldjango = { "djlint" },
 			},
 		},
 	},
